af52731897c61e029c2b732910d6265bc91f9617
Samantha <samantha.fenton@live.co.uk>
2023-07-26 13:36:54 +0100

    server running

--- server.js
+++ server.js
@@ -10,28 +10,27 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8090;
 
-app.get("/", (request, response)=>{
+app.get("/", (request, response) => {
   response.status(200).json("hello");
 });
 
-app.get("/photos", async (request, response)=>{
-    const API = `https://api.unsplash.com/search/photos/?client_id=${process.env.ACCESS_KEY}&query=sam`;
-    const response = await axios.get(API);
-    console.log(response.addTrailers.results[0].url.regular);
-    response.status(200).json("testing");
-    const photos = res.data.results.map(photos)=>{
-        return{
-            id: photos.id,
-            img_url:photos.url.regular,
-            original_image: photos.links.self,
-            photographer: photos.user.name,
-        }
-        };
-        response.status(200).json(photos);
-    });
+app.get("/photos", async (request, response) => {
+  const API = `https://api.unsplash.com/search/photos/?client_id=${process.env.ACCESS_KEY}&query=sam`;
+  const res = await axios.get(API);
+  //console.log(response.data.results[0].url.regular);
 
-})
+  const photos = res.data.results.map((photo) => {
+    return {
+      id: photo.id,
+      img_url: photo.urls.regular,
+      original_image: photo.links.self,
+      photographer: photo.user.name,
+    };
+  });
+  response.status(200).json(photos);
+});
+//response.status(200).json("hello");
 
-app.listen(PORT, ()=>(`app is listening on ${PORT}`));
+app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
 
 //https://api.unsplash.com/search/photos/?client_id=YOUR_ACCESS_KEY
